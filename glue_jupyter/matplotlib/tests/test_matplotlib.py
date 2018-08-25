7443da3b309c590c67dbe5df64fcafcee242e924
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-08-25 13:52:12 -0400

    Added test to make sure the Matplotlib viewers run

---
+++ glue_jupyter/matplotlib/tests/test_matplotlib.py
@@ -0,0 +1,14 @@
+import os
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+DATA = os.path.join(os.path.dirname(__file__), 'data')
+
+
+def test_matplotlib():
+
+    with open(os.path.join(DATA, 'matplotlib.ipynb')) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep.preprocess(nb, {'metadata': {'path': DATA}})
