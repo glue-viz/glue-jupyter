29c1c09acd4587bd1ab008aa786194448bd2b82f
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-08-26 08:27:46 -0400

    Fix kernel name on Python 2

--- glue_jupyter/matplotlib/tests/test_matplotlib.py
+++ glue_jupyter/matplotlib/tests/test_matplotlib.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
@@ -10,5 +11,10 @@ def test_matplotlib():
     with open(os.path.join(DATA, 'matplotlib.ipynb')) as f:
         nb = nbformat.read(f, as_version=4)
 
-    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    if sys.version_info[0] < 3:
+        kernel_name = 'python2'
+    else:
+        kernel_name = 'python3'
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name=kernel_name)
     ep.preprocess(nb, {'metadata': {'path': DATA}})
