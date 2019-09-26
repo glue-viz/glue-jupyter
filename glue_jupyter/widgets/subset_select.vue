bd5a91d76a521ca8d83aefc88bf147b7158177f2
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2019-09-26 10:08:53 +0200

    refactor: convert SubsetSelect to use VuetifyTemplate
    - sync functions are now 1 on 1 mappings between glue state and ui
    - output_widget is not used because sync functions are now very
      simple.

---
+++ glue_jupyter/widgets/subset_select.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-menu>
+        <template #activator="{ on }">
+            <v-btn v-on="on" text v-if="selected.length > 0">
+                <v-icon left :color="selected[0].color">signal_cellular_4_bar</v-icon>
+                {{ selected[0].label }}
+            </v-btn>
+            <v-btn v-on="on" text v-else>{{ no_selection_text }}</v-btn>
+        </template>
+        <v-list>
+            <v-list-item @click="selected = []">
+                <v-list-item-title>
+                    {{ no_selection_text }}
+                </v-list-item-title>
+            </v-list-item>
+            <v-list-item v-for="item of available" @click="selected = [item]">
+                <v-list-item-avatar>
+                    <v-icon :color="item.color">signal_cellular_4_bar</v-icon>
+                </v-list-item-avatar>
+                <v-list-item-title>
+                    {{ item.label }}
+                </v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
