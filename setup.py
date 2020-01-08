04f021df4caec3c645ecd5bc6501f8ab3f6284c3
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-08 17:51:05 +0000

    Added pyproject.toml file

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import pip
 import setuptools
 from distutils.version import LooseVersion
 from setuptools import setup
@@ -10,12 +9,6 @@ from setuptools import setup
 if LooseVersion(setuptools.__version__) < LooseVersion('30.3.0'):
     sys.stderr.write("ERROR: glue-jupyter requires setuptools 30.3.0 or "
                      "later (found {0})".format(setuptools.__version__))
-    sys.exit(1)
-
-# pip 18 or later is needed for setup.cfg install_requires URLs
-if LooseVersion(pip.__version__) < LooseVersion('18'):
-    sys.stderr.write("ERROR: glue-jupyter requires pip 18 or "
-                     "later (found {0})".format(pip.__version__))
     sys.exit(1)
 
 setup(use_scm_version=True)
