aee2837d4b9cca7169b7e3e5e74e81a16416e04a
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2020-03-19 19:15:24 +0100

    refactor: use ipyvuetify for ProfileViewerState

--- glue_jupyter/common/state_widgets/viewer_profile.py
+++ glue_jupyter/common/state_widgets/viewer_profile.py
@@ -1,28 +1,30 @@
-from ipywidgets import VBox, ToggleButton, Checkbox
-
-from ...widgets.linked_dropdown import LinkedDropdown
-from ...link import link
+import ipyvuetify as v
+import traitlets
+from ...state_traitlets_helpers import GlueState
+from ...vuetify_helpers import load_template, link_glue_choices
 
 __all__ = ['ProfileViewerStateWidget']
 
 
-class ProfileViewerStateWidget(VBox):
+class ProfileViewerStateWidget(v.VuetifyTemplate):
+    template = load_template('viewer_profile.vue', __file__)
+
+    glue_state = GlueState().tag(sync=True)
+
+    reference_data_items = traitlets.List().tag(sync=True)
+    reference_data_selected = traitlets.Int(allow_none=True).tag(sync=True)
+
+    x_att_items = traitlets.List().tag(sync=True)
+    x_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
+
+    function_items = traitlets.List().tag(sync=True)
+    function_selected = traitlets.Int(allow_none=True).tag(sync=True)
 
     def __init__(self, viewer_state):
+        super().__init__()
 
-        self.state = viewer_state
+        self.glue_state = viewer_state
 
-        self.widget_show_axes = Checkbox(value=True, description="Show axes")
-        link((self.widget_show_axes, 'value'), (self.state, 'show_axes'))
-
-        self.widget_reference_data = LinkedDropdown(self.state, 'reference_data', label='reference')
-        self.widget_axis_x = LinkedDropdown(self.state, 'x_att', label='x axis')
-        self.widget_function = LinkedDropdown(self.state, 'function', label='function')
-
-        self.button_normalize = ToggleButton(value=False, description='normalize',
-                                             tooltip='Normalize profiles')
-        link((self.button_normalize, 'value'), (self.state, 'normalize'))
-
-        super().__init__([self.widget_reference_data,
-                          self.widget_axis_x, self.widget_function,
-                          self.button_normalize, self.widget_show_axes])
+        link_glue_choices(self, viewer_state, 'reference_data')
+        link_glue_choices(self, viewer_state, 'x_att')
+        link_glue_choices(self, viewer_state, 'function')
