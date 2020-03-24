9822a10cf74622f0c3cfd9a3b3e3b9735f97e505
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2020-03-24 15:22:18 +0100

    refactor: use ipyvuetify for ImageLayerState

--- glue_jupyter/common/state_widgets/layer_image.py
+++ glue_jupyter/common/state_widgets/layer_image.py
@@ -1,78 +1,61 @@
-from ipywidgets import (Checkbox, FloatSlider, ColorPicker, VBox, Dropdown,
-                        FloatText)
+from ipywidgets import Checkbox, FloatSlider, ColorPicker, VBox, Dropdown
 from glue.config import colormaps
 from glue.utils import color2hex
 
 from ...link import link
-from ...widgets import LinkedDropdown
+
+import ipyvuetify as v
+import traitlets
+from ...state_traitlets_helpers import GlueState
+from ...vuetify_helpers import load_template, link_glue_choices, link_glue
 
 __all__ = ['ImageLayerStateWidget', 'ImageSubsetLayerStateWidget']
 
 
-class ImageLayerStateWidget(VBox):
+class ImageLayerStateWidget(v.VuetifyTemplate):
+    template = load_template('layer_image.vue', __file__)
+
+    glue_state = GlueState().tag(sync=True)
+
+    attribute_items = traitlets.List().tag(sync=True)
+    attribute_selected = traitlets.Int(allow_none=True).tag(sync=True)
+
+    stretch_items = traitlets.List().tag(sync=True)
+    stretch_selected = traitlets.Int(allow_none=True).tag(sync=True)
+
+    percentile_items = traitlets.List().tag(sync=True)
+    percentile_selected = traitlets.Int(allow_none=True).tag(sync=True)
+
+    colormap_items = traitlets.List().tag(sync=True)
+    color_mode = traitlets.Unicode().tag(sync=True)
 
     def __init__(self, layer_state):
-
-        self.state = layer_state
-
-        self.widget_visible = Checkbox(description='visible', value=self.state.visible)
-        link((self.state, 'visible'), (self.widget_visible, 'value'))
-
-        self.widget_attribute = LinkedDropdown(self.state, 'attribute', label='attribute')
-
-        self.widget_opacity = FloatSlider(min=0, max=1, step=0.01, value=self.state.alpha,
-                                          description='opacity')
-        link((self.state, 'alpha'), (self.widget_opacity, 'value'))
-
-        self.widget_contrast = FloatSlider(min=0, max=4, step=0.01, value=self.state.contrast,
-                                           description='contrast')
-        link((self.state, 'contrast'), (self.widget_contrast, 'value'))
-
-        self.widget_bias = FloatSlider(min=0, max=1, step=0.01, value=self.state.bias,
-                                       description='bias')
-        link((self.state, 'bias'), (self.widget_bias, 'value'))
-
-        self.widget_stretch = LinkedDropdown(self.state, 'stretch', label='stretch')
-
-        self.widget_percentile = LinkedDropdown(self.state, 'percentile', ui_name='limits',
-                                                label='percentile')
-
-        self.widget_v_min = FloatText(description='min', value=self.state.v_min)
-        link((self.state, 'v_min'), (self.widget_v_min, 'value'))
-
-        self.widget_v_max = FloatText(description='max', value=self.state.v_max)
-        link((self.state, 'v_max'), (self.widget_v_max, 'value'))
-
-        self.widget_color = ColorPicker(description='color')
-        link((self.state, 'color'), (self.widget_color, 'value'), color2hex)
-
-        self.widget_colormap = Dropdown(options=colormaps.members, description='colormap')
-        link((self.state, 'cmap'), (self.widget_colormap, 'value'))
-
         super().__init__()
 
-        self.state.viewer_state.add_callback('color_mode', self.setup_widgets)
+        self.layer_state = layer_state
+        self.glue_state = layer_state
 
-        self.setup_widgets()
+        link_glue_choices(self, layer_state, 'attribute')
+        link_glue_choices(self, layer_state, 'stretch')
+        link_glue_choices(self, layer_state, 'percentile')
 
-    def setup_widgets(self, *args):
+        self.colormap_items = [dict(
+            text=cmap[0],
+            value=cmap[1].name
+        ) for cmap in colormaps.members]
 
-        children = [self.widget_visible, self.widget_opacity]
+        link_glue(self, 'color_mode', layer_state.viewer_state)
 
-        children.append(self.widget_attribute)
-        children.append(self.widget_contrast)
-        children.append(self.widget_bias)
-        children.append(self.widget_stretch)
-        children.append(self.widget_percentile)
-        children.append(self.widget_v_min)
-        children.append(self.widget_v_max)
+    def vue_set_colormap(self, data):
+        print(f'{data}')
 
-        if self.state.viewer_state.color_mode == 'Colormaps':
-            children.append(self.widget_colormap)
-        else:
-            children.append(self.widget_color)
+        cmap = None
+        for member in colormaps.members:
+            if member[1].name == data:
+                cmap = member[1]
+                break
 
-        self.children = children
+        self.layer_state.cmap = cmap
 
 
 class ImageSubsetLayerStateWidget(VBox):
---
+++ glue_jupyter/common/state_widgets/layer_image.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div>
+            <v-select label="attribute" :items="attribute_items" v-model="attribute_selected" hide-details class="margin-bottom: 16px" />
+        </div>
+        <div>
+            <v-subheader class="pl-0 slider-label">opacity</v-subheader>
+            <v-slider max="1" step="0.01" :value="glue_state.alpha" @end="setAlpha" hide-details />
+        </div>
+        <div>
+            <v-subheader class="pl-0 slider-label">contrast</v-subheader>
+            <v-slider max="4" step="0.01" :value="glue_state.contrast" @end="setContrast" hide-details />
+        </div>
+        <div>
+            <v-subheader class="pl-0 slider-label">bias</v-subheader>
+            <v-slider max="1" step="0.01" :value="glue_state.bias" @end="setBias" hide-details />
+        </div>
+        <div>
+            <v-select label="stretch" :items="stretch_items" v-model="stretch_selected" hide-details />
+        </div>
+        <div>
+            <v-select label="percentile" :items="percentile_items" v-model="percentile_selected" hide-details />
+        </div>
+        <div>
+            <v-text-field label="min" v-model.number="glue_state.v_min" type="number" hide-details />
+        </div>
+        <div>
+            <v-text-field label="max" v-model.number="glue_state.v_max" type="number" hide-details />
+        </div>
+        <div v-if="color_mode === 'Colormaps'">
+            <v-select label="colormap" :items="colormap_items" :value="glue_state.cmap" @change="set_colormap" />
+        </div>
+    </div>
+</template>
+<script>
+    modules.export = {
+        methods: {
+            /* TODO: use @change with debounce instead of @end */
+            setAlpha(value) {
+                this.glue_state.alpha = value;
+            },
+            setContrast(value) {
+                this.glue_state.contrast = value;
+            },
+            setBias(value) {
+                this.glue_state.bias = value;
+            },
+        },
+    }
+</script>
+<style id="layer_image">
+    .vuetify-styles .v-subheader.slider-label {
+        font-size: 12px;
+        height: 16px;
+    }
+</style>
