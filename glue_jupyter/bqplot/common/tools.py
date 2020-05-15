6820b97fa0f138df4d07dc77837fdc434f69d087
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-05-15 13:26:28 +0100

    Remove unused code

--- glue_jupyter/bqplot/common/tools.py
+++ glue_jupyter/bqplot/common/tools.py
@@ -156,7 +156,6 @@ class BqplotCircleMode(InteractCheckableTool):
                     roi = CircularROI(xc=xc, yc=yc, radius=rx)
                 else:
                     roi = EllipticalROI(xc=xc, yc=yc, radius_x=rx, radius_y=ry)
-                print(roi)
                 self.viewer.apply_roi(roi)
                 if self.finalize_callback is not None:
                     self.finalize_callback()
