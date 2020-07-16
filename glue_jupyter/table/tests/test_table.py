a3adec61d84a5a9f01d83b6ae220eccdda83d8d2
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-07-16 13:27:20 +0200

    fix: TableViewer can work with no data, will show the last if multiple
    Fixes #194

--- glue_jupyter/table/tests/test_table.py
+++ glue_jupyter/table/tests/test_table.py
@@ -1,4 +1,7 @@
-def test_table(app, dataxyz):
+from glue_jupyter.table import TableViewer
+
+
+def test_table_filter(app, dataxyz):
     table = app.table(data=dataxyz)
     assert len(table.layers) == 1
     assert table.widget_table is not None
@@ -13,3 +16,30 @@ def test_table(app, dataxyz):
     app.subset('test', dataxyz.id['x'] > 1)
     assert len(table.layers) == 3
     assert len(table.widget_table.selections) == 2
+
+
+def test_table_add_remove_data(app, dataxyz, dataxz):
+    table = app.new_data_viewer(TableViewer, data=None, show=True)
+    assert len(table.layers) == 0
+    assert table.widget_table.total_length == 0
+    table.add_data(dataxyz)
+    assert table.widget_table.items, "table should fill automatically"
+    assert table.widget_table.items[0]['z'] == dataxyz['z'][0]
+    assert table.widget_table.total_length, "total length should grow"
+
+    assert dataxz['z'][0] != dataxyz['z'][0], "we assume this to check data changes in the table"
+
+    table.add_data(dataxz)
+    assert table.widget_table.data is dataxz
+    assert table.widget_table.items[0]['z'] == dataxz['z'][0]
+    assert len(table.layers) == 2
+
+    table.remove_data(dataxz)
+    assert table.widget_table.data is dataxyz
+    assert table.widget_table.items[0]['z'] == dataxyz['z'][0]
+    assert len(table.layers) == 1
+
+    table.remove_data(dataxyz)
+    assert table.widget_table.data is None
+    assert table.widget_table.items == []
+    assert len(table.layers) == 0
