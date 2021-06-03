f64c06f21a34422adfefa314b6b793540ae46501
Maarten A. Breddels <maartenbreddels@gmail.com>
2021-06-03 16:11:53 +0200

    more formats

--- glue_jupyter/common/state_widgets/tests/test_viewer_image.py
+++ glue_jupyter/common/state_widgets/tests/test_viewer_image.py
@@ -4,9 +4,9 @@ def test_contour_levels(app, data_image):
     layer_state = widget_state.layer_state
     layer_state.level_mode = "Custom"
 
-    widget_state.c_levels_txt = '1e2, 1e3'
-    assert layer_state.levels == [100, 1000]
-    assert widget_state.c_levels_txt == '1e2, 1e3'
+    widget_state.c_levels_txt = '1e2, 1e3, 0.1, .1'
+    assert layer_state.levels == [100, 1000, 0.1, 0.1]
+    assert widget_state.c_levels_txt == '1e2, 1e3, 0.1, .1'
 
     layer_state.levels = [10, 100]
     assert widget_state.c_levels_txt == '10, 100'
