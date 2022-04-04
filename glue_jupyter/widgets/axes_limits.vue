f3d98013a301945e10eddd5f1e3fd8e017630191
Kyle Conroy <kyleconroy@gmail.com>
2022-04-04 12:14:24 -0400

    Apply suggestions from code review
    Co-authored-by: Mario Buikhuizen <mbuikhuizen@gmail.com>

--- glue_jupyter/widgets/axes_limits.vue
+++ glue_jupyter/widgets/axes_limits.vue
@@ -1,18 +1,20 @@
 <template>
-  <v-row>
-    <v-col cols=6>
-      <glue-float-field label="xmin" :value.sync="glue_state.x_min" />
-    </v-col cols=6>
-    <v-col>
-      <glue-float-field label="xmax" :value.sync="glue_state.x_max" />
+  <div>
+  <v-row no-gutters>
+    <v-col cols="6">
+      <glue-float-field class="mr-1" label="xmin" :value.sync="glue_state.x_min" />
+    </v-col>
+    <v-col  cols="6">
+      <glue-float-field class="ml-1" label="xmax" :value.sync="glue_state.x_max" />
     </v-col>
   </v-row>
-  <v-row>
-    <v-col cols=6>
-      <glue-float-field label="ymin" :value.sync="glue_state.y_min" />
+  <v-row no-gutters>
+    <v-col cols="6">
+      <glue-float-field class="mr-1" label="ymin" :value.sync="glue_state.y_min" />
     </v-col>
-    <v-col cols=6>
-      <glue-float-field label="ymax" :value.sync="glue_state.y_max" />
+    <v-col cols="6">
+      <glue-float-field class="ml-1" label="ymax" :value.sync="glue_state.y_max" />
     </v-col>
   </v-row>
+  </div>
 </template>
