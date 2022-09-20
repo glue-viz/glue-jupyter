1636fcc3e262b18705a29a04ea99944a32d897fa
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-09-20 14:59:49 +0200

    fix: do not upload float64 data to the widget, case to float32
    WebGL cannot handle float64 data, so we need to cast it to float32,
    if not, the frontend will do it for us, but that will be a waste
    of bandwith.
    
    Addressses: https://github.com/spacetelescope/jdaviz/issues/1367

--- glue_jupyter/bqplot/image/frb_mark.py
+++ glue_jupyter/bqplot/image/frb_mark.py
@@ -93,6 +93,8 @@ class FRBImage(ImageGL):
 
         # Get the array and assign it to the artist
         image = self.array_maker(bounds=bounds)
+        if image.dtype == np.dtype("float64"):
+            image = image.astype(np.float32)
         if image is not None:
             with self.hold_sync():
                 self.image = image
