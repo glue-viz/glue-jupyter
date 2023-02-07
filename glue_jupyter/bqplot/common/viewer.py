ad9447871153ec87a3f5809127d7d3535f70c555
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-02-07 10:22:07 +0000

    Don't set scale limits if glue state limits are None, to avoid unecessary updates

--- glue_jupyter/bqplot/common/viewer.py
+++ glue_jupyter/bqplot/common/viewer.py
@@ -114,13 +114,15 @@ class BqplotBaseView(IPyWidgetView):
         # doesn't change this - at the end of the day, the two scales are
         # separate widgets so will result in two updates.
 
-        with self.scale_x.hold_sync():
-            self.scale_x.min = float_or_none(self.state.x_min)
-            self.scale_x.max = float_or_none(self.state.x_max)
+        if self.state.x_min is not None and self.state.x_max is not None:
+            with self.scale_x.hold_sync():
+                self.scale_x.min = float_or_none(self.state.x_min)
+                self.scale_x.max = float_or_none(self.state.x_max)
 
-        with self.scale_y.hold_sync():
-            self.scale_y.min = float_or_none(self.state.y_min)
-            self.scale_y.max = float_or_none(self.state.y_max)
+        if self.state.y_min is not None and self.state.y_max is not None:
+            with self.scale_y.hold_sync():
+                self.scale_y.min = float_or_none(self.state.y_min)
+                self.scale_y.max = float_or_none(self.state.y_max)
 
         self._last_limits = (self.state.x_min, self.state.x_max,
                              self.state.y_min, self.state.y_max)
