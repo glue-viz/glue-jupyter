693d72bd4778a06024579753dc19d25615a05464
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-09-12 11:16:12 +0100

    Only sync limits with glue state when 'Apply limits' button is clicked

--- glue_jupyter/widgets/axes_limits.py
+++ glue_jupyter/widgets/axes_limits.py
@@ -1,4 +1,6 @@
+import traitlets
 import ipyvuetify as v
+from echo import delay_callback
 from ..state_traitlets_helpers import GlueState
 
 __all__ = ['AxesLimits']
@@ -9,6 +11,23 @@ class AxesLimits(v.VuetifyTemplate):
 
     glue_state = GlueState().tag(sync=True)
 
+    x_min = traitlets.Float(default_value=None, allow_none=True).tag(sync=True)
+    x_max = traitlets.Float(default_value=None, allow_none=True).tag(sync=True)
+    y_min = traitlets.Float(default_value=None, allow_none=True).tag(sync=True)
+    y_max = traitlets.Float(default_value=None, allow_none=True).tag(sync=True)
+
     def __init__(self, viewer_state):
         super().__init__()
         self.glue_state = viewer_state
+
+    def vue_apply_limits(self, data):
+        self._cache = (self.x_min, self.x_max, self.y_min, self.y_max)
+        with delay_callback(self.glue_state, 'x_min', 'x_max', 'y_min', 'y_max'):
+            if self.x_min is not None:
+                self.glue_state.x_min = self.x_min
+            if self.x_max is not None:
+                self.glue_state.x_max = self.x_max
+            if self.y_min is not None:
+                self.glue_state.y_min = self.y_min
+            if self.y_max is not None:
+                self.glue_state.y_max = self.y_max
--- glue_jupyter/widgets/axes_limits.vue
+++ glue_jupyter/widgets/axes_limits.vue
@@ -2,18 +2,25 @@
   <div>
   <v-row no-gutters>
     <v-col cols="6">
-      <glue-float-field class="mr-1" label="xmin" :value.sync="glue_state.x_min" />
+      <glue-float-field class="mr-1" label="xmin" :value.sync="x_min" />
     </v-col>
     <v-col  cols="6">
-      <glue-float-field class="ml-1" label="xmax" :value.sync="glue_state.x_max" />
+      <glue-float-field class="ml-1" label="xmax" :value.sync="x_max" />
     </v-col>
   </v-row>
   <v-row no-gutters>
     <v-col cols="6">
-      <glue-float-field class="mr-1" label="ymin" :value.sync="glue_state.y_min" />
+      <glue-float-field class="mr-1" label="ymin" :value.sync="y_min" />
     </v-col>
     <v-col cols="6">
-      <glue-float-field class="ml-1" label="ymax" :value.sync="glue_state.y_max" />
+      <glue-float-field class="ml-1" label="ymax" :value.sync="y_max" />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col class="text-center" cols="12">
+    <v-btn @click="apply_limits">
+      Apply limits
+    </v-btn>
     </v-col>
   </v-row>
   </div>
