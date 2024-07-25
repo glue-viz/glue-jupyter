2d81936c499a4a5632c1a67bc1174064cb1bb988
Carifio24 <carifio24@gmail.com>
2024-07-25 15:36:21 -0400

    Apply similar coordinate changes to scatter viewer.

--- glue_jupyter/ipyvolume/scatter/layer_artist.py
+++ glue_jupyter/ipyvolume/scatter/layer_artist.py
@@ -95,9 +95,9 @@ class IpyvolumeScatterLayerArtist(LayerArtist):
 
     def update(self):
         # we don't use layer, but layer.data to get everything
-        self.scatter.x = ensure_numerical(self.layer.data[self._viewer_state.x_att]).ravel()
-        self.scatter.z = ensure_numerical(self.layer.data[self._viewer_state.y_att]).ravel()
+        self.scatter.z = ensure_numerical(self.layer.data[self._viewer_state.x_att]).ravel()
         self.scatter.y = ensure_numerical(self.layer.data[self._viewer_state.z_att]).ravel()
+        self.scatter.x = ensure_numerical(self.layer.data[self._viewer_state.y_att]).ravel()
         self.quiver.x = self.scatter.x
         self.quiver.z = self.scatter.y
         self.quiver.y = self.scatter.z
@@ -123,9 +123,9 @@ class IpyvolumeScatterLayerArtist(LayerArtist):
 
     def _update_quiver(self):
         with self.quiver.hold_sync():
-            self.quiver.vx = self.layer.data[self.state.vx_att].ravel()
-            self.quiver.vz = self.layer.data[self.state.vy_att].ravel()
+            self.quiver.vz = self.layer.data[self.state.vx_att].ravel()
             self.quiver.vy = self.layer.data[self.state.vz_att].ravel()
+            self.quiver.vx = self.layer.data[self.state.vy_att].ravel()
 
     def _update_size(self):
         size = self.state.size
