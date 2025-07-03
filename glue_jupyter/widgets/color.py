debb3528ed2c180a1c540fe11bdb95783f5ff81f
Carifio24 <carifio24@gmail.com>
2025-07-03 15:11:29 -0400

    Make color widget more general.

--- glue_jupyter/widgets/color.py
+++ glue_jupyter/widgets/color.py
@@ -13,25 +13,31 @@ class Color(widgets.VBox):
         super(Color, self).__init__(**kwargs)
         self.state = state
 
+        self.cmap_att = kwargs.get('cmap_att', 'cmap_att')
+        self.cmap_mode_attr = kwargs.get('cmap_mode_attr', 'cmap_mode')
+
         self.widget_color = widgets.ColorPicker(description='color')
         link((self.state, 'color'), (self.widget_color, 'value'), color2hex)
 
-        cmap_mode_options = type(self.state).cmap_mode.get_choice_labels(self.state)
+        cmap_mode_options = getattr(type(self.state), self.cmap_mode_attr).get_choice_labels(self.state)
         self.widget_cmap_mode = widgets.RadioButtons(options=cmap_mode_options,
                                                      description='cmap mode')
-        link((self.state, 'cmap_mode'), (self.widget_cmap_mode, 'value'))
-
-        self.widget_cmap_att = LinkedDropdown(self.state, 'cmap_att',
-                                              ui_name='color attribute',
-                                              label='color attribute')
-
-        self.widget_cmap_vmin = widgets.FloatText(description='color min')
-        self.widget_cmap_vmax = widgets.FloatText(description='color max')
-        self.widget_cmap_v = widgets.VBox([self.widget_cmap_vmin, self.widget_cmap_vmax])
-        link((self.state, 'cmap_vmin'), (self.widget_cmap_vmin, 'value'), lambda value: value or 0)
-        link((self.state, 'cmap_vmax'), (self.widget_cmap_vmax, 'value'), lambda value: value or 1)
+        link((self.state, self.cmap_mode_attr), (self.widget_cmap_mode, 'value'))
+        
+        children = [self.widget_cmap_mode, self.widget_color]
+        if self.cmap_att is not None:
+          self.widget_cmap_att = LinkedDropdown(self.state, 'cmap_att',
+                                                ui_name='color attribute',
+                                                label='color attribute')
+          self.widget_cmap_vmin = widgets.FloatText(description='color min')
+          self.widget_cmap_vmax = widgets.FloatText(description='color max')
+          self.widget_cmap_v = widgets.VBox([self.widget_cmap_vmin, self.widget_cmap_vmax])
+          link((self.state, 'cmap_vmin'), (self.widget_cmap_vmin, 'value'), lambda value: value or 0)
+          link((self.state, 'cmap_vmax'), (self.widget_cmap_vmax, 'value'), lambda value: value or 1)
+          children.extend((self.widget_cmap_att, self.widget_cmap_v))
 
         self.widget_cmap = widgets.Dropdown(options=colormaps, description='colormap')
+        children.append(self.widget_cmap)
         link((self.state, 'cmap'), (self.widget_cmap, 'label'),
              lambda cmap: colormaps.name_from_cmap(cmap), lambda name: colormaps[name])
 
@@ -39,10 +45,9 @@ class Color(widgets.VBox):
               lambda value: None if value == cmap_mode_options[0] else 'none')
         dlink((self.widget_cmap_mode, 'value'), (self.widget_cmap.layout, 'display'),
               lambda value: None if value == cmap_mode_options[1] else 'none')
-        dlink((self.widget_cmap_mode, 'value'), (self.widget_cmap_att.layout, 'display'),
-              lambda value: None if value == cmap_mode_options[1] else 'none')
-        dlink((self.widget_cmap_mode, 'value'), (self.widget_cmap_v.layout, 'display'),
-              lambda value: None if value == cmap_mode_options[1] else 'none')
-        self.children = (self.widget_cmap_mode, self.widget_color,
-                         self.widget_cmap_att, self.widget_cmap_v,
-                         self.widget_cmap)
+        if self.cmap_att is not None:
+            dlink((self.widget_cmap_mode, 'value'), (self.widget_cmap_att.layout, 'display'),
+                  lambda value: None if value == cmap_mode_options[1] else 'none')
+            dlink((self.widget_cmap_mode, 'value'), (self.widget_cmap_v.layout, 'display'),
+                  lambda value: None if value == cmap_mode_options[1] else 'none')
+        self.children = tuple(children)
