5f0d0be5157dd75016cf1d2c7be16d11afab4ddd
CyclingNinja <bennett.sm89@gmail.com>
2025-11-12 11:18:51 +0000

    Adds glue-axes template

--- glue_jupyter/common/state_widgets/layer_scatter.vue
+++ glue_jupyter/common/state_widgets/layer_scatter.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="glue-layer-scatter">
+        <div class="text-subtitle-2 font-weight-bold"></divclass>
         <div class="text-subtitle-2 font-weight-bold">Color</div>
         <div>
             <v-select label="color" :items="cmap_mode_items" v-model="cmap_mode_selected" hide-details />
         </div>
         <div>
-            <glue-axes/>
+          <v-subheader class="pl-0 slider-label">Axes definition</v-subheader>
+          <Glue-axes/>
         </div>
         <template v-if="glue_state.cmap_mode === 'Linear'">
             <div>
@@ -109,12 +111,12 @@
     </div>
 </template>
 <script>
-import {defineComponent} from "vue";
-import Glue_axes from "../../widgets/glue_axes.vue";
+<!--import {defineComponent} from "vue";-->
+<!--import Glue_axes from "../../widgets/glue_axes.vue";-->
 
-export default defineComponent({
-  components: {Glue_axes}
-})
+<!--export default defineComponent({-->
+<!--  components: {Glue_axes}-->
+<!--})-->
 </script>
 <style id="layer_scatter">
 .glue-layer-scatter .v-subheader.slider-label {
--- glue_jupyter/widgets/glue_axes.vue
+++ glue_jupyter/widgets/glue_axes.vue
@@ -1,27 +1,42 @@
 <template>
-  <v-container class="pa-4" max-width="1080">
-    <v-card class="pa-4">
-      <v-card-title>Set Figure Bounds</v-card-title>
-      <v-card-text>
+  <div>
         <v-row dense>
-          <v-col cols="3">
-            <v-text-field v-model="xmin" label="X Min" type="number" outlined />
+          <v-col cols="5">
+            <glue-float-field label="xmin" :value.sync="glue_state.x_min"/>
           </v-col>
 
-          <v-col cols="3">
-            <v-text-field v-model="xmax" label="X Max" type="number" outlined />
+          <v-col cols="5">
+            <glue-float-field label="xmax" :value.sync="glue_state.x_max"/>
           </v-col>
         </v-row>
+
         <v-row dense>
-          <v-col cols="3">
-            <v-text-field v-model="ymin" label="Y Min" type="number" outlined />
+          <v-col cols="5">
+            <glue-float-field label="ymin" :value.sync="glue_state.y_min"/>
           </v-col>
 
-          <v-col cols="3">
-            <v-text-field v-model="ymax" label="Y Max" type="number" outlined />
+          <v-col cols="5">
+            <glue-float-field label="ymax" :value.sync="glue_state.y_max"/>
           </v-col>
         </v-row>
-      </v-card-text>
-    </v-card>
-  </v-container>
+  </div>
 </template>
+
+<script>
+    /* `glue_state` is linked to the text-field and `value` will only be updated when it's a valid number
+       This means that any formatting such as '1e3' will be kept.
+       Only when `value` is changed externally, `glue_state` will be update according to the current `value`.
+    */
+    module.exports = {
+        props: { glue_state: { type: Object, default: () => ({ x_min: 0, x_max: 1, y_min: 0, y_max: 1 }) } },
+        watch: {
+            glue_state: { handler(value) {
+                // when we change the input, we only update value when valid
+                // and we emit the change only when we have a valid number
+                this.$emit('update:value', value);
+            },
+              deep: true
+            }
+            },
+    }
+</script>
