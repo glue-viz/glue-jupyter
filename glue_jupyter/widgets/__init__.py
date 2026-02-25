b31c5b9d10a4acaf4cba822fb80d846eaf522d10
Carifio24 <carifio24@gmail.com>
2026-02-25 01:23:29 -0500

    Add Python-usable float field widget.

--- glue_jupyter/widgets/__init__.py
+++ glue_jupyter/widgets/__init__.py
@@ -1,5 +1,6 @@
 from .color import Color
 from .size import Size
 from .linked_dropdown import LinkedDropdown
+from .glue_float_field import GlueFloatField
 
-__all__ = ['Color', 'Size', 'LinkedDropdown']
+__all__ = ['Color', 'Size', 'LinkedDropdown', 'GlueFloatField']
