bd07f2fa5cd31fbc30c2fefb751ff79457f09273
Mario Buikhuizen <mariobuikhuizen@widgetti.io>
2026-05-19 15:52:33 +0200

    fix: correct background for toolbar buttons

--- glue_jupyter/common/basic_jupyter_toolbar.vue
+++ glue_jupyter/common/basic_jupyter_toolbar.vue
@@ -2,7 +2,7 @@
     <v-btn-toggle v-model="active_tool_id" class="transparent">
         <v-tooltip v-for="[id, {tooltip, img}] of Object.entries(tools_data)" location="bottom">
             <template v-slot:activator="{ props }">
-                <v-btn v-bind="props" icon :value="id">
+                <v-btn v-bind="props" icon variant="text" :value="id">
                     <img :src="img" width="20"/>
                 </v-btn>
             </template>
